ecaba75c71246c85b5fb93455913673ea8615046
waseemusman17 <waseeem.usman17@gmail.com>
2020-11-17 18:19:11 +0500

    Home Complete

--- assets/js/custom.js
+++ assets/js/custom.js
@@ -3,16 +3,35 @@ $(document).ready(function(){
         loop:true,
         margin:10,
         nav:true,
+        dots:false,
         responsive:{
             0:{
-                items:1
+                items:1,
+                nav:false
             },
             600:{
-                items:3
+                items:3,
+                nav:false
             },
             1000:{
+                items:4,
+                nav:false
+            },
+            1200:{
                 items:4
             }
         }
     })
+
+
+
+     // On Click Scroll to next section
+     $('.scrollDiv').on('click', function () {
+        var ele = $('.scrollSection');
+        // this will search within the section
+        $("html, body").animate({
+             scrollTop: $(ele).offset().top
+        }, 1000);
+        return false;
+    });
 });
